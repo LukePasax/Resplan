05f6a7c34a479ccab2c31b72261fa50a461ce64c
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-03-31 15:28:09 +0200

    First implementation of Channel

--- app/src/main/java/daw/core/channel/RSChannel.java
+++ app/src/main/java/daw/core/channel/RSChannel.java
@@ -1,7 +1,21 @@
 package daw.core.channel;
 
+import net.beadsproject.beads.ugens.Gain;
+
 /**
  * This interface models a channel, that is a...
  */
 public interface RSChannel {
+
+    void addInput(Gain g);
+
+    void setVolume(int vol);
+
+    int getVolume();
+
+    void enable();
+
+    void disable();
+
+    boolean isEnabled();
 }
