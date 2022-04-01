3f2cede97f3b7dd358d521a23d86defe49486b6d
Luca Pasini <pasini.luca09@gmail.com>
2022-04-01 15:38:42 +0200

    Add Javadoc to RPMIxer

--- app/src/main/java/daw/core/mixer/RPMixer.java
+++ app/src/main/java/daw/core/mixer/RPMixer.java
@@ -6,22 +6,50 @@ import net.beadsproject.beads.core.AudioContext;
 
 import java.util.List;
 
+/**
+ * This interface models a Mixer,
+ * which is a container and controller for channels.
+ */
+
 public interface RPMixer {
 
+    /**
+     * Sets the AudioContext for the mixer.
+     * @param context the context used in the Mixer
+     */
     void setAudioContext(AudioContext context);
 
+    /**
+     * @return the AudioContext for the mixer.
+     */
     AudioContext getAudioContext();
 
+    /**
+     * @return a list of Channels in the Mixer
+     */
     List<RPChannel> getChannels();
 
+    /**
+     * A method to create a Channel in the Mixer.
+     */
     void createChannel();
 
+    /**
+     * @param clip the clip contained in a channel in the Mixer
+     * @return the channel that contains the clip
+     */
     RPChannel getChannel(RPClip clip);
 
     // wait for Return Channel interface
+
+    /**
+     * A method to link the output of a Channel to the Input of a ReturnChannel.
+     * @param channel the channel which output is to be linked
+     */
     void linkChannel(RPChannel channel);
 
+    /**
+     * A method to create a ReturnChannel in the Mixer.
+     */
     void createReturnChannel();
-
-
 }
---
+++ app/src/main/java/daw/core/mixer/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains all the Interfaces and Classes related to the RPMixer.
+ */
+package daw.core.mixer;
