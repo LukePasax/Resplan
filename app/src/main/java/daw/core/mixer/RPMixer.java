c980d66afc0456a9a5fe26e8fc4e6340eecfe9c7
Luca Pasini <pasini.luca09@gmail.com>
2022-04-01 14:35:27 +0200

    Create RPMixer interface

--- app/src/main/java/daw/core/mixer/RPMixer.java
+++ app/src/main/java/daw/core/mixer/RPMixer.java
@@ -1,9 +1,27 @@
 package daw.core.mixer;
 
+import daw.core.channel.RPChannel;
+import daw.core.clip.RPClip;
 import net.beadsproject.beads.core.AudioContext;
+
+import java.util.List;
 
 public interface RPMixer {
 
+    void setAudioContext(AudioContext context);
+
     AudioContext getAudioContext();
+
+    List<RPChannel> getChannels();
+
+    void createChannel();
+
+    RPChannel getChannel(RPClip clip);
+
+    // wait for Return Channel interface
+    void linkChannel(RPChannel channel);
+
+    void createReturnChannel();
+
 
 }
