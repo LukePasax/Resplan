97a02e7389aacbff44f9f3dd09756c73caf5358b
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-04-01 16:16:12 +0200

    Add implementation of MasterChannel

---
+++ app/src/main/java/daw/core/channel/MasterChannel.java
@@ -0,0 +1,36 @@
+package daw.core.channel;
+
+import net.beadsproject.beads.ugens.Gain;
+
+public class MasterChannel implements RPChannel {
+
+    @Override
+    public void addInput(Gain g) {
+        
+    }
+
+    @Override
+    public void setVolume(int vol) {
+
+    }
+
+    @Override
+    public int getVolume() {
+        return 0;
+    }
+
+    @Override
+    public void enable() {
+
+    }
+
+    @Override
+    public void disable() {
+
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
