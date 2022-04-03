27f7360dfc085b2fbdd4f0976dee1be80334e91d
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-04-03 14:12:18 +0200

    Add mock implementation of BasicProcessingUnit

---
+++ app/src/main/java/daw/core/channel/BasicProcessingUnit.java
@@ -0,0 +1,58 @@
+package daw.core.channel;
+
+import net.beadsproject.beads.core.UGen;
+import net.beadsproject.beads.ugens.Gain;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+public class BasicProcessingUnit implements ProcessingUnit {
+
+    private final List<UGen> effects = new LinkedList<>();
+
+    @Override
+    public List<UGen> getEffects() {
+        return null;
+    }
+
+    @Override
+    public UGen getEffectAtPosition(int index) {
+        return null;
+    }
+
+    @Override
+    public void addEffect(UGen u) {
+
+    }
+
+    @Override
+    public void addEffectAtPosition(UGen u, int index) {
+
+    }
+
+    @Override
+    public void moveEffect(int currentIndex, int newIndex) {
+
+    }
+
+    @Override
+    public void swapEffects(int firstIndex, int secondIndex) {
+
+    }
+
+    @Override
+    public void replace(int index, UGen u) {
+
+    }
+
+    @Override
+    public void addInput(Gain g) {
+
+    }
+
+    @Override
+    public Optional<Gain> getOutput() {
+        return Optional.empty();
+    }
+
+}
