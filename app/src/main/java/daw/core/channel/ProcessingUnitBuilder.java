b751d1df22b213a56752bd7fac28ba19be256b69
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-04-05 10:50:59 +0200

    Add interface ProcessingUnitBuilder

---
+++ app/src/main/java/daw/core/channel/ProcessingUnitBuilder.java
@@ -0,0 +1,27 @@
+package daw.core.channel;
+
+/**
+ * Makes the creation of a {@link ProcessingUnit} interactive,
+ * in that the client can specify any number of effects in any order.
+ * The only state which raises an {@link IllegalStateException} when the method build is called
+ * is the one does not contain any effect, because a {@link ProcessingUnit} can never be empty.
+ */
+public interface ProcessingUnitBuilder {
+
+    ProcessingUnitBuilder filter();
+
+    ProcessingUnitBuilder panner();
+
+    ProcessingUnitBuilder delay();
+
+    ProcessingUnitBuilder compressor();
+
+    ProcessingUnitBuilder reverb();
+
+    ProcessingUnitBuilder sidechain();
+
+    ProcessingUnitBuilder gate();
+
+    ProcessingUnit build() throws IllegalStateException;
+
+}
