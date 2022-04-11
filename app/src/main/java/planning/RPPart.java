4c7163280e80810a5d0fc381a9dc68f1caec997c
Gabriele Menghi <gabriele.menghi@studio.unibo.it>
2022-04-11 12:40:54 +0200

    Add new interface Element with basic methods

--- app/src/main/java/planning/RPPart.java
+++ app/src/main/java/planning/RPPart.java
@@ -1,6 +1,6 @@
 package planning;
 
-/*
+/**
  * It's an interface that represents a clip in high level which could belong to a guest or a soundtrack
  */
 public interface RPPart {
--- app/src/main/java/planning/RPRole.java
+++ app/src/main/java/planning/RPRole.java
@@ -1,7 +1,8 @@
 package planning;
 
-/*
+/**
  * It's an interface that represents a guest or a soundtrack in high level
  */
 public interface RPRole {
+	
 }
