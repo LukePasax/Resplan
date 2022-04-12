3a56f4da36bce6ca95de8b1f675f59f0b6933648
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-04-12 18:42:17 +0200

    Add three specific implementations of role

---
+++ app/src/main/java/planning/SoundtrackRole.java
@@ -0,0 +1,9 @@
+package planning;
+
+public class SoundtrackRole extends RoleImpl {
+
+	public SoundtrackRole(final String title, final String description) {
+		super(title, description, RoleType.SOUNDTRACK);
+	}
+
+}
---
+++ app/src/main/java/planning/SpeechRole.java
@@ -0,0 +1,9 @@
+package planning;
+
+public class SpeechRole extends RoleImpl {
+
+	public SpeechRole(final String title, final String description) {
+		super(title, description, RoleType.SPEECH);
+	}
+
+}
