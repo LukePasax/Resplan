c5faac846d74e6dd65e12246d1a7f05eb8f51cdc
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-04-14 16:47:10 +0200

    First non-mock implementation of Gate and Sidechain

--- app/src/main/java/daw/core/effect/Gate.java
+++ app/src/main/java/daw/core/effect/Gate.java
@@ -1,24 +1,30 @@
 package daw.core.effect;
 
+import Resplan.AudioContextManager;
 import net.beadsproject.beads.core.AudioContext;
 import net.beadsproject.beads.core.UGen;
 import net.beadsproject.beads.data.DataBead;
 import net.beadsproject.beads.data.DataBeadReceiver;
+import net.beadsproject.beads.ugens.Compressor;
 
 public class Gate extends UGen implements DataBeadReceiver {
 
-    public Gate(AudioContext context) {
-        super(context);
+    private Compressor compressor;
+
+    public Gate(float threshold, float attack, float decay, float ratio) {
+        super(AudioContextManager.getAudioContext());
+        this.compressor = new Compressor(AudioContextManager.getAudioContext()).setThreshold(threshold).
+                setAttack(attack).setDecay(decay).setRatio(ratio);
     }
 
     @Override
     public void calculateBuffer() {
-
+        this.compressor.calculateBuffer();
     }
 
     @Override
     public DataBeadReceiver sendData(DataBead db) {
-        return null;
+        return this.compressor.sendData(db);
     }
 
 }
