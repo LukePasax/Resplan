b4b43f08632ea52bb859803a3e202240ee7a740e
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-04-15 14:47:00 +0200

    Add Javadoc in Element.java

--- app/src/main/java/planning/Element.java
+++ app/src/main/java/planning/Element.java
@@ -4,6 +4,18 @@ package planning;
  * It's an interface that represents a generic element of high level DAW
  */
 public interface Element {
+	
+	/**
+	 * Returns the title of an element
+	 * 
+	 * @return the string representing the string of an element
+	 */
 	String getTitle();
+	
+	/**
+	 * Returns the description of an element
+	 * 
+	 * @return the string representing the description of an element
+	 */
 	String getDescription();
 }
