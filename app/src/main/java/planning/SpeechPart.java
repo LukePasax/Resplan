7be2eb4b2dad068c92b7c781a54e29799467c720
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-04-15 10:10:59 +0200

    Implementation of SpeechPart.java

--- app/src/main/java/planning/SpeechPart.java
+++ app/src/main/java/planning/SpeechPart.java
@@ -1,9 +1,25 @@
 package planning;
 
+import java.util.Optional;
+
 public class SpeechPart extends PartImpl {
+	
+	private Optional<Text> text = Optional.empty();
 
 	public SpeechPart(String title, String description, PartType type) {
 		super(title, description, PartType.SPEECH);
+	}
+	
+	public void addText(final Text text) {
+		this.text = Optional.of(text);
+	}
+	
+	public boolean isTextPresent() {
+		return this.text.isPresent();
+	}
+	
+	public Text getText() {
+		return this.text.get();
 	}
 
 }
