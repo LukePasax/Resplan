ee3542369bd85d78007504691b21db17096169de
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-04-16 16:43:17 +0200

    Add interface Speaker.java

---
+++ app/src/main/java/planning/Speaker.java
@@ -0,0 +1,10 @@
+package planning;
+
+/**
+ * This interface represents a speaker who can participate to a project
+ */
+public interface Speaker {
+	int getSpeakerCode();
+	String getFirstName();
+	String getLastName();
+}
