8f2a510dcac90744361650e3efc032520464f7ca
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-04-16 16:47:30 +0200

    Add simple implementation of a speaker

---
+++ app/src/main/java/planning/SimpleSpeaker.java
@@ -0,0 +1,48 @@
+package planning;
+
+import java.util.Objects;
+
+public class SimpleSpeaker implements Speaker {
+	
+	private int speakerCode;
+	private String firstName;
+	private String lastName;
+
+	public SimpleSpeaker(final int speakerCode, final String firstName, final String lastName) {
+		this.speakerCode = speakerCode;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	@Override
+	public int getSpeakerCode() {
+		return this.speakerCode;
+	}
+
+	@Override
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	@Override
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(speakerCode);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SimpleSpeaker other = (SimpleSpeaker) obj;
+		return speakerCode == other.speakerCode;
+	}
+}
