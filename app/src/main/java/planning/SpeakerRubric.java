0ab84bad34a951e8aa4a28ce04f0f7066194c653
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-04-16 17:05:24 +0200

    Add interface SpeakerRubric.java

---
+++ app/src/main/java/planning/SpeakerRubric.java
@@ -0,0 +1,16 @@
+package planning;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+/**
+ * This interface models a rubric that contains all the speakers saved before
+ */
+public interface SpeakerRubric {
+	List<Speaker> getSpeakers();
+	boolean addSpeaker(Speaker speaker);
+	void removeSpeaker(Speaker speaker);
+	Optional<Speaker> searchSpeaker(int speakerCode);
+	List<Speaker> getFilteredSpeakers(Predicate<? super Speaker> filter);
+}
