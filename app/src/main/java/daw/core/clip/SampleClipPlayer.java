d0b0dc59514f6053eb6946f598b8b9594f154ba9
Alessandro <alessandro.antonini6@studio.unibo.it>
2022-04-20 11:37:34 +0200

    SampleClipPlayer first implementation

---
+++ app/src/main/java/daw/core/clip/SampleClipPlayer.java
@@ -0,0 +1,51 @@
+package daw.core.clip;
+
+import java.io.IOException;
+
+import net.beadsproject.beads.data.Sample;
+import net.beadsproject.beads.data.audiofile.FileFormatException;
+import net.beadsproject.beads.data.audiofile.OperationUnsupportedException;
+import net.beadsproject.beads.ugens.SamplePlayer;
+
+public class SampleClipPlayer implements RPClipPlayer {
+	
+	private final SamplePlayer player;
+	private final double contentPosition;
+	
+	public SampleClipPlayer(RPClip sampleClip) throws IOException, OperationUnsupportedException, FileFormatException {
+		if(sampleClip.getClass()!=SampleClip.class) {
+			throw new IllegalArgumentException("The supplied clip must be a SampleClip class object.");
+		}
+		this.player = new SamplePlayer(new Sample(sampleClip.getContent().getAbsolutePath()));
+		this.contentPosition = sampleClip.getContentPosition();
+		this.player.setPosition(this.contentPosition);
+		this.stop();
+	}
+
+	@Override
+	public void play() {
+		this.player.start();
+	}
+
+	@Override
+	public void pause() {
+		this.player.pause(true);
+	}
+
+	@Override
+	public void stop() {
+		this.pause();
+		this.setPlaybackPosition(contentPosition);
+	}
+
+	@Override
+	public void setPlaybackPosition(double milliseconds) {
+		this.player.setPosition(milliseconds);
+	}
+
+	@Override
+	public double getPlaybackPosition() {
+		return this.player.getPosition();
+	}
+
+}
