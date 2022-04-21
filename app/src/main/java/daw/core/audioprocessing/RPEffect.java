82abf8e1aa750fbb28a04e8393a9ee8b8fd8f8a2
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-04-21 21:58:52 +0200

    Complete javadoc for ChannelFactory, Gate and RPEffect

--- app/src/main/java/daw/core/audioprocessing/RPEffect.java
+++ app/src/main/java/daw/core/audioprocessing/RPEffect.java
@@ -3,6 +3,10 @@ package daw.core.audioprocessing;
 import net.beadsproject.beads.core.AudioContext;
 import net.beadsproject.beads.core.UGen;
 
+/**
+ * This class is an extension of {@link UGen}. In the context of this software, this class is the one
+ * all effects must extend. Non-abstract subclasses must provide an implementation for method calculateBuffer.
+ */
 public abstract class RPEffect extends UGen {
 
     public RPEffect(AudioContext context, int ins, int outs) {
--- app/src/main/java/daw/core/channel/ChannelFactory.java
+++ app/src/main/java/daw/core/channel/ChannelFactory.java
@@ -2,17 +2,19 @@ package daw.core.channel;
 
 import daw.core.audioprocessing.ProcessingUnit;
 import net.beadsproject.beads.core.UGen;
+import daw.core.audioprocessing.CompressorWithSidechaining;
+import daw.core.audioprocessing.Gate;
 
 /**
  * This factory allows the client to acquire a particular form of {@link RPChannel}.
- * The channels are only distinguished by their {@link ProcessingUnit} (TO DO: their default Volume?).
- * Still, every form of built-in channel can be modulated by adding {@link net.beadsproject.beads.core.UGen}s
+ * The channels are only distinguished by their {@link ProcessingUnit}.
+ * Still, every form of built-in channel can be later modulated by adding {@link UGen}s
  * that serve as effects for sound processing.
  */
 public interface ChannelFactory {
 
     /**
-     * Creates a {@link RPChannel} that does not possess a {@link ProcessingUnit}.
+     * Creates an {@link RPChannel} that does not possess a {@link ProcessingUnit}.
      * This is channel represents the most modular choice when creating a channel,
      * as no built-in structure is provided.
      * @return the {@link RPChannel}.
@@ -20,25 +22,30 @@ public interface ChannelFactory {
     RPChannel basic();
 
     /**
-     * Creates a gated {@link RPChannel}.
+     * Creates a gated {@link RPChannel}. To know what a gate is and when to use it,
+     * please read the documentation at {@link Gate}.
      * @return the {@link RPChannel}.
      */
     RPChannel gated();
 
     /**
-     * Creates a sidechained {@link RPChannel}.
+     * Creates an {@link RPChannel} that possesses a sidechain. To know what a sidechain is and how to use it,
+     * please read the documentation at {@link CompressorWithSidechaining}.
      * @return the {@link RPChannel}.
      */
     RPChannel sidechained(UGen u);
 
     /**
-     * Creates a return channel. A return channel is... TO DO
+     * Creates a return channel.
+     * Return channels let the client run multiple sounds through the same effect in the mixer.
+     * Using return channels can make the sound more cohesive.
      * @return the {@link RPChannel}.
      */
     RPChannel returnChannel();
 
     /**
-     * Creates a master channel. A master channel is... TO DO
+     * Creates a master channel. A master channel is the one to which all other {@link RPChannel}s'
+     * outputs are sent to.
      * @return the {@link RPChannel}.
      */
     RPChannel masterChannel();
