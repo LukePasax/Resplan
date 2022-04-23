078e7c5bca15a17f97d3f4e0d0bbc042f6d0c4d2
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-04-23 11:38:28 +0200

    Rethink implementation of BasicProcessingUnitBuilder

--- app/src/main/java/daw/core/audioprocessing/BasicProcessingUnitBuilder.java
+++ app/src/main/java/daw/core/audioprocessing/BasicProcessingUnitBuilder.java
@@ -1,26 +1,16 @@
 package daw.core.audioprocessing;
 
 import net.beadsproject.beads.core.UGen;
-
-import javax.swing.text.html.Option;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class BasicProcessingUnitBuilder implements ProcessingUnitBuilder {
 
     private Optional<Sidechaining> sidechain;
-    private Optional<RPEffect> lowPassFilter;
-    private Optional<RPEffect> highPassFilter;
-    private Optional<RPEffect> reverb;
-    private Optional<RPEffect> gate;
-    private Optional<RPEffect> compressor;
+    private final List<RPEffect> effects;
 
     public BasicProcessingUnitBuilder() {
-        this.lowPassFilter = Optional.empty();
-        this.highPassFilter = Optional.empty();
-        this.reverb = Optional.empty();
         this.sidechain = Optional.empty();
-        this.gate = Optional.empty();
+        this.effects = new ArrayList<>();
     }
 
     @Override
@@ -33,57 +23,44 @@ public class BasicProcessingUnitBuilder implements ProcessingUnitBuilder {
 
     @Override
     public ProcessingUnitBuilder lowPassFilter(int channels, float cutoffFrequency) {
-        if (this.lowPassFilter.isEmpty()) {
-            this.lowPassFilter = Optional.of(new LowPassFilter(channels, cutoffFrequency));
-        }
+        this.effects.add(new LowPassFilter(channels, cutoffFrequency));
         return this;
     }
 
     @Override
     public ProcessingUnitBuilder highPassFilter(int channels, float cutoffFrequency) {
-        if (this.highPassFilter.isEmpty()) {
-            this.highPassFilter = Optional.of(new HighPassFilter(channels, cutoffFrequency));
-        }
+        this.effects.add(new HighPassFilter(channels, cutoffFrequency));
         return this;
     }
 
     @Override
     public ProcessingUnitBuilder reverb(int channels) {
-        if (this.reverb.isEmpty()) {
-            this.reverb = Optional.of(new DigitalReverb(channels));
-        }
+        this.effects.add(new DigitalReverb(channels));
         return this;
     }
 
     @Override
     public ProcessingUnitBuilder gate(int channels) {
-        if (this.gate.isEmpty()) {
-            this.gate = Optional.of(new Gate(channels));
-        }
+        this.effects.add(new Gate(channels));
         return this;
     }
 
     @Override
     public ProcessingUnitBuilder compressor(int channels) {
-        if (this.compressor.isEmpty()) {
-            this.compressor = Optional.of(new Compression(channels));
-        }
+        this.effects.add(new Compression(channels));
         return this;
     }
 
     @Override
     public ProcessingUnit build() throws IllegalStateException {
-        final List<Optional<RPEffect>> effects = new ArrayList<>(List.of(this.lowPassFilter, this.highPassFilter,
-                this.compressor, this.gate, this.reverb));
-        if (effects.stream().anyMatch(Optional::isPresent)) {
-            final var pu = new BasicProcessingUnit(effects.stream().filter(Optional::isPresent).
-                    map(Optional::get).collect(Collectors.toList()));
+        if (!this.effects.isEmpty()) {
+            final var pu = new BasicProcessingUnit(effects);
             if (this.sidechain.isPresent()) {
                 pu.addSidechaining(this.sidechain.get());
             }
             return pu;
         }
-        throw new IllegalStateException();
+        throw new IllegalStateException("Cannot create an empty ProcessingUnit.");
     }
 
 }
