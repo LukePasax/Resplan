8c131a4788571dcd10ce816e68ca839bbbfec9ae
Alessandro <alessandro.antonini6@studio.unibo.it>
2022-05-02 16:28:53 +0200

    Start writing RPClip JavaDoc

--- app/src/main/java/daw/core/clip/RPClip.java
+++ app/src/main/java/daw/core/clip/RPClip.java
@@ -3,21 +3,35 @@ package daw.core.clip;
 import java.io.File;
 
 /**
- * @author alessandro
- * This interface models a clip, that is a container for each audio/MIDI file you want to place in the timeline of some tape channel.
- * A clip let you specify the audio content play-back starting point and its duration.
+ * An RPClip is a container for each content (such as an audio/MIDI file) you want to 
+ * add to any {@link RPTapeChannel}.
+ * <p>For every clip you can specify it's duration, it's content and the content position.
+ * <br>The duration of a clip could be different than it's content duration, so you can select 
+ * the section of the content you want to fill the clip adjusting the duration with {@link #setDuration} 
+ * and the content starting position with {@link #setContentPosition}.
+ * <p>A clip could also be empty. You can verify it calling {@link #isEmpty} method.
+ * An empty RPclip is for reserving space in any {@link RPTapeChannel}, so you can fill it later.
+ * <p>You can get an exact copy of any clip with it's content calling the {@link #duplicate} method.
  */
 public interface RPClip {
 	
-	public static final double DEFAULT_DURATION = 1000;
-	
 	/**
-	 * @param milliseconds
+	 * This should be the default duration for an RPClip if it has not been specified or is not yet known.
+	 */
+	public static final double DEFAULT_DURATION = 5000;
+	
+	/** 
+	 * Set the RPClip duration.
+	 * 
+	 * @param milliseconds The duration of the clip in milliseconds.
+	 * 
+	 * @throws IllegalArgumentException If the specified duration is negative.
 	 */
 	void setDuration(double milliseconds);
 
 	/**
-	 * @param milliseconds
+	 * Set the content starting point.
+	 * @param milliseconds The content starting time in milliseconds. The specified time must be within the duration of the content or 
 	 */
 	void setContentPosition(double milliseconds);
 	
