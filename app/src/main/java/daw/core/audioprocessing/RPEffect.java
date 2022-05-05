fe88d10c6a2268016253c869c9f9cd1d1287933e
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-05-05 23:02:25 +0200

    Fix javadoc for RPEffect

--- app/src/main/java/daw/core/audioprocessing/RPEffect.java
+++ app/src/main/java/daw/core/audioprocessing/RPEffect.java
@@ -20,10 +20,29 @@ public abstract class RPEffect extends UGen {
         super(context, ins, outs);
     }
 
+    /**
+     * Allows getting the value associated to each parameter of this particular effect.
+     * Parameters influence how the sound is processed by this effect.
+     * @return a {@link Map} where the keys are the parameters and the values are the
+     * current value of each parameter of the effect.
+     */
     public abstract Map<String, Float> getParameters();
 
+    /**
+     * Modifies the value of all the parameters specified as keys in the given map. Those parameters
+     * will then contain the value associated to them in the map.
+     * The keys that do not match any of the effect's parameters are ignored
+     * and so are the values associated to them.
+     * @param parameters the {@link Map} that contains the parameters that must be modified.
+     */
     public abstract void setParameters(Map<String, Float> parameters);
 
+    /**
+     * Allows getting the default value of a certain parameter of this particular effect.
+     * @param key a parameter of this effect.
+     * @return the floating-point default value of the parameter.
+     * @throws IllegalArgumentException if the given string does not match any of the parameters of this effect.
+     */
     protected abstract float getDefaultValue(String key);
 
     /**
