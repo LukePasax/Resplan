5c86e3932f98d95be2e43ef60e8a9f722bb67baa
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-05-10 09:45:40 +0200

    Fix javadoc for Sidechaining

--- app/src/main/java/daw/core/audioprocessing/Sidechaining.java
+++ app/src/main/java/daw/core/audioprocessing/Sidechaining.java
@@ -7,8 +7,8 @@ import net.beadsproject.beads.ugens.Compressor;
 
 /**
  * Sidechaining is a compression technique that makes the sound coming in from a source
- * “duck” the output signal of another source. It is useful when the volume of a source needs to be tied
- * to the volume of another source, so that the sounds don't overlap.
+ * duck the output signal of another source. It is useful when the volume of a source needs to be tied
+ * to the volume of another source, so that the sounds do not overlap.
  * Practically, sidechaining is a property that a {@link ProcessingUnit} may possess.
  * A {@link ProcessingUnit} that is sidechained gets an input from another audio source to calculate the
  * level of compression. This external audio source can always be changed after initialization, by calling
