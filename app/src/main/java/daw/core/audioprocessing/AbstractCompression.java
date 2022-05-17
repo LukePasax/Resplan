e4fb2a755d26a24f4adb5eec891e567be843c0f5
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-05-17 11:40:46 +0200

    Refactor Compression, Gate and Sidechaining in AbstractCompression

---
+++ app/src/main/java/daw/core/audioprocessing/AbstractCompression.java
@@ -0,0 +1,85 @@
+package daw.core.audioprocessing;
+
+import daw.utilities.AudioContextManager;
+import net.beadsproject.beads.data.DataBead;
+import net.beadsproject.beads.ugens.Compressor;
+
+import java.util.Map;
+
+public abstract class AbstractCompression extends RPEffect {
+
+    protected final Compressor compressor;
+
+    public AbstractCompression(int channels) {
+        super(AudioContextManager.getAudioContext(), channels, channels);
+        this.compressor = new Compressor(AudioContextManager.getAudioContext(), channels);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @return a {@link Map} where the keys are the parameters and the values are the
+     * current value of each parameter of the effect.
+     */
+    @Override
+    public Map<String, Float> getParameters() {
+        return Map.of("threshold", this.compressor.getThreshold(), "ratio", this.compressor.getRatio(),
+                "attack", this.compressor.getAttack(), "decay", this.compressor.getDecay());
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param parameters the {@link Map} that contains the parameters that must be modified.
+     */
+    @Override
+    public void setParameters(Map<String, Float> parameters) {
+        this.compressor.sendData(new DataBead(parameters));
+    }
+
+    /**
+     * Allows to get the number of input channels of the effect.
+     * @return an integer that represents the number of input.
+     */
+    @Override
+    public int getIns() {
+        return this.compressor.getIns();
+    }
+
+    /**
+     * Allows to get the number of output channels of the effect.
+     * @return an integer that represents the number of outputs.
+     */
+    @Override
+    public int getOuts() {
+        return this.compressor.getOuts();
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param key a parameter of this effect.
+     * @return the floating-point default value of the parameter.
+     * @throws IllegalArgumentException if the given string does not match any of the parameters of this effect.
+     */
+    // TODO
+    @Override
+    public float getDefaultValue(String key) {
+        return 0.0f;
+    }
+
+    /**
+     * Sets a default value for the parameter specified by the given key. If there is no such parameter,
+     * this method does nothing.
+     *
+     * @param key   the name of a parameter.
+     * @param value the value that the key has as default after this method is called.
+     */
+    @Override
+    public void setDefaultValue(String key, float value) {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public abstract void calculateBuffer();
+
+}
--- app/src/main/java/daw/core/audioprocessing/BasicSidechaining.java
+++ app/src/main/java/daw/core/audioprocessing/BasicSidechaining.java
@@ -15,19 +15,16 @@ import java.util.Map;
  * instead of an interface), this implementation of {@link Sidechaining} is the only one currently supported.
  * This means all clients must use this class when sidechaining is needed. </p>
  */
-public class BasicSidechaining extends UGen implements Sidechaining, AudioElement {
-
-    private final Compressor compressor;
+public class BasicSidechaining extends AbstractCompression implements Sidechaining, AudioElement {
 
     /**
      * Constructs a {@link BasicSidechaining} object with the given parameters.
-     * Remember to pass the correct sidechained channel, because it cannot be changed after the instantiation.
      * @param u the channel to be sidechained.
      * @param channels the number of inputs and outputs of the given channel.
      */
     public BasicSidechaining(UGen u, int channels) {
-        super(AudioContextManager.getAudioContext(), channels, channels);
-        this.compressor = new Compressor(AudioContext.getDefaultContext(), channels).setSideChain(u);
+        super(channels);
+        this.compressor.setSideChain(u);
     }
 
     /**
@@ -40,68 +37,11 @@ public class BasicSidechaining extends UGen implements Sidechaining, AudioElemen
     }
 
     /**
-     * @return a {@link Map} where the keys are the parameters and the values are the
-     * current value of each parameter of the effect.
-     */
-    public Map<String, Float> getParameters() {
-        return Map.of("threshold", this.compressor.getThreshold(), "ratio", this.compressor.getRatio(),
-                "attack", this.compressor.getAttack(), "decay", this.compressor.getDecay());
-    }
-
-    /**
-     * @param parameters the {@link Map} that contains the parameters that must be modified.
-     */
-    public void setParameters(Map<String, Float> parameters) {
-        this.compressor.sendData(new DataBead(parameters));
-    }
-
-    /**
-     * Allows to get the number of input channels of the effect.
-     * @return an integer that represents the number of input.
-     */
-    @Override
-    public int getIns() {
-        return this.compressor.getIns();
-    }
-
-    /**
-     * Allows to get the number of output channels of the effect.
-     * @return an integer that represents the number of outputs.
-     */
-    @Override
-    public int getOuts() {
-        return this.compressor.getOuts();
-    }
-
-    /**
-     * Allows getting the default value of a certain parameter of this particular effect.
-     *
-     * @param key a parameter of this effect.
-     * @return the floating-point default value of the parameter.
-     * @throws IllegalArgumentException if the given string does not match any of the parameters of this effect.
-     */
-    @Override
-    public float getDefaultValue(String key) {
-        return 0;
-    }
-
-    /**
-     * Sets a default value for the parameter specified by the given key. If there is no such parameter,
-     * this method does nothing.
-     *
-     * @param key   the name of a parameter.
-     * @param value the value that the key has as default after this method is called.
-     */
-    @Override
-    public void setDefaultValue(String key, float value) {
-
-    }
-
-    /**
      * {@inheritDoc}
      */
     @Override
     public void calculateBuffer() {
         this.compressor.calculateBuffer();
     }
+
 }
