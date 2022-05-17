bf3d2d029e7de3c111454f63eeff443f37b7212c
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-05-17 10:12:04 +0200

    Create MixerDeserializer and add it to JacksonDeserializer

--- app/src/main/java/controller/storing/JacksonDeserializer.java
+++ app/src/main/java/controller/storing/JacksonDeserializer.java
@@ -3,15 +3,18 @@ package controller.storing;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import daw.core.mixer.RPMixer;
 
 public abstract class JacksonDeserializer<T> implements Deserializer<T> {
 
-    private final ObjectMapper mapper;
+    protected final ObjectMapper mapper;
 
     public JacksonDeserializer() {
         this.mapper = new JsonMapper().builder()
                 .build()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .registerModule(new SimpleModule().addDeserializer(RPMixer.class, new MixerDeserializer()));
     }
 
     @Override
