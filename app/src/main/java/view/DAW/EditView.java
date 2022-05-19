a1fc3ad03244898bdf4c8aa976d0d75013a9a833
Alessandro <alessandro.antonini6@studio.unibo.it>
2022-05-19 14:54:32 +0200

    Edit Layout panels and creating DragResizer for channels.

--- app/src/main/java/view/DAW/EditView.java
+++ app/src/main/java/view/DAW/EditView.java
@@ -4,20 +4,67 @@ import java.io.FileInputStream;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Side;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class EditView extends Application {
 	
 	private final static String SEP = System.getProperty("file.separator");
-
+		NumberAxis x = new NumberAxis();
+		
 	@Override
 	public void start(Stage stage) throws Exception {
 		FXMLLoader loader = new FXMLLoader();
 		String path = System.getProperty("user.dir") + SEP + "src" + SEP + "main" + SEP + "resources" + SEP + "view" + SEP + "EditView.fxml";
 		FileInputStream fxmlStream = new FileInputStream(path);
 		VBox root = (VBox) loader.load(fxmlStream);
+		
+		
+		Button zoomIn = new Button("+");
+		Button zoomOut = new Button("-");
+		
+		//chart test
+
+		x.setSide(Side.TOP);
+		CategoryAxis y = new CategoryAxis();
+		y.setSide(Side.RIGHT);
+		
+		ScatterChart<Number,String> c = new ScatterChart<>(x,y);
+		
+		Series<Number,String> s = new Series<>();
+		Data<Number,String> d = new Data<>(10, "vocals");
+		d.setNode(new Label("puttana"));
+		s.getData().add(d);
+		s.getData().add(new Data<>(30, "vocals"));
+		s.getData().add(new Data<>(1, "vocals"));
+		s.getData().add(new Data<>(10, "fx"));
+		s.getData().add(new Data<>(12, "fx"));
+		s.getData().add(new Data<>(5, "fx"));
+		
+		c.getData().add(s);
+		//root.getChildren().addAll(c, zoomIn, zoomOut);
+		
+		zoomIn.setOnAction(e->{
+			NumberAxis xa = (NumberAxis) c.getXAxis();
+			x.setAutoRanging(false);
+			xa.setLowerBound(xa.getLowerBound()+10);
+			xa.setUpperBound(xa.getUpperBound()-10);
+			
+		});
+		zoomOut.setOnAction(e->{
+			NumberAxis xa = (NumberAxis) c.getXAxis();
+			xa.setAutoRanging(true);
+		});
+		
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("FXML with controller");
--- app/src/main/java/view/DAW/EditViewController.java
+++ app/src/main/java/view/DAW/EditViewController.java
@@ -4,63 +4,59 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
-
 import javafx.fxml.*;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.*;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
-import javafx.scene.text.Text;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
 
 public class EditViewController implements Initializable{
 	
 	private Map<Pane, String> channels = new HashMap<>(); 
 	
 	Map<Double, Region> clips = new HashMap<>();
-			
+	
+	/**
+	 * La griglia usata per dimensionare la timeline alla vista dei canali.
+	 */
+	@FXML
+	private GridPane timelineToChannelsAligner;
+	
+	/**
+	 * SplitPanel per dimensionare la divisione tra channel info e channel content.
+	 */
+	@FXML
+	private SplitPane channelsInfoResizer;		
+	/**
+	 * Asse del tempo.
+	 */
 	@FXML
 	private NumberAxis timeAxis;
 	
+	/**
+	 * VBox per contenuto canali
+	 */
 	@FXML
-	private SplitPane channelsPane;
-	
+	private VBox channelsContentPane;
+		
+	/**
+	 * VBox per info canali
+	 */
 	@FXML
-	private SplitPane channelsLabelsPane;
+	private VBox channelsInfoPane;
 	
+	/**
+	 * Aggiungi canale bottone
+	 */
 	@FXML
 	private MenuItem addChannel;
-	
-	@FXML
-	public void addChannel() {
-		Pane newChannel = new Pane();
-		newChannel.setMinHeight(30);
-		newChannel.setMaxHeight(140);
-		channels.put(newChannel, "Channel");
-		
-		channelsPane.getItems().add(newChannel);
-		
-		Pane newLabel = new Pane();
-		newLabel.setMinHeight(30);
-		newLabel.setMaxHeight(140);
-		newLabel.getChildren().add(new Label("Channel"));
-		channelsLabelsPane.getItems().add(newLabel);
-	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		//resize channels labels pane with channels pane
-		channelsPane.needsLayoutProperty().addListener((obs, old, needsLayout) -> {
-			if(!needsLayout) {
-				int i = 0;
-				for(Double d : channelsPane.getDividerPositions()) {
-					channelsLabelsPane.setDividerPosition(i, d);
-					i++;
-				}	
-			}
+		//channel content - info - timeline grid resize
+		channelsInfoResizer.needsLayoutProperty().addListener((obs, old, needsLayout) -> {
+			timelineToChannelsAligner.getColumnConstraints().get(1).setPercentWidth((1-(channelsInfoResizer.getDividerPositions()[0]))*100);
 		});
-		
-		//disable resize for channels labels pane
-		channelsLabelsPane.setDisable(true);
 		
 		/*
 		timeAxis.needsLayoutProperty().addListener((obs, old, needsLayout) -> {
@@ -74,4 +70,28 @@ public class EditViewController implements Initializable{
         });*/
 	}
 	
+	/**
+	 * Aggiunta di un canale.
+	 */
+	@FXML
+	public void addChannel() {
+		//comunica al controller che vuoi creare un canale.
+		
+		//infos
+		Pane newChannelInfos = new Pane();
+		newChannelInfos.setBorder(new Border(new BorderStroke(null, null, Paint.valueOf("#999999"), null, 
+				null, null, BorderStrokeStyle.SOLID, null, CornerRadii.EMPTY, null, null)));
+		DragResizer.makeResizable(newChannelInfos);
+		channelsInfoPane.getChildren().add(newChannelInfos);
+		
+		newChannelInfos.getChildren().add(new Label("Channel"));
+		//clip panel
+		Pane newChannel = new Pane();
+		newChannel.setBorder(new Border(new BorderStroke(null, null, Paint.valueOf("#999999"), null, 
+				null, null, BorderStrokeStyle.SOLID, null, CornerRadii.EMPTY, null, null)));
+		newChannelInfos.needsLayoutProperty().addListener((obs, old, needsLayout) -> {
+			newChannel.setMinHeight(newChannelInfos.getHeight());
+		});
+		channelsContentPane.getChildren().add(newChannel);	
+	}
 }
