09bf695b7ecff33f9694fb93c98ae8386dccf305
Luca Pasini <pasini.luca09@gmail.com>
2022-05-19 15:21:26 +0200

    Add controller for newClip

---
+++ app/src/main/java/controller/view/planning/NewClipController.java
@@ -0,0 +1,12 @@
+package controller.view.planning;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class NewClipController {
+    public TextField clipTitleSelection;
+    public TextField clipDescriptionSelection;
+    public Button newClipConfirm;
+    public Button clipFilePicker;
+    public TextField clipFileUrl;
+}
