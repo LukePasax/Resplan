7c4a57377c74357906ad55d13a094a7e935d5b34
Luca Pasini <pasini.luca09@gmail.com>
2022-05-28 17:42:56 +0200

    Fix scene change size problems

--- app/src/main/java/Resplan/App.java
+++ app/src/main/java/Resplan/App.java
@@ -4,9 +4,11 @@ import controller.general.Controller;
 import controller.general.ControllerImpl;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import view.common.ViewData;
 import view.common.ViewDataImpl;
@@ -27,28 +29,27 @@ public class App extends Application {
     	return viewData;
     }
 
+    private Scene activeScene;
+    private Parent sleepingRoot;
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader planningLoader = new FXMLLoader(getClass().getResource("/view/planningView.fxml"));
-        Scene planningScene = new Scene(planningLoader.load());
+        this.activeScene = new Scene(planningLoader.load());
         App.getController().setPlanningController(planningLoader.getController());
-        stage.setScene(planningScene);
+        stage.setScene(activeScene);
         stage.getIcons().add(new Image("/images/icon.png"));
         stage.setTitle("Resplan");
         stage.show();
+        FXMLLoader editLoader = new FXMLLoader(getClass().getResource("/view/EditView.fxml"));
+        this.sleepingRoot = (Parent) editLoader.load();
+        activeScene.setOnKeyPressed(this::switchScene);
+    }
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/EditView.fxml"));
-        Scene editScene = new Scene(loader.load());
-        planningScene.setOnKeyPressed(e->{
-        	if(e.getCode().equals(KeyCode.A)) {
-        		stage.setScene(editScene);
-        	}
-        	
-        });
-        editScene.setOnKeyPressed(e->{
-        	if(e.getCode().equals(KeyCode.A)) {
-            	stage.setScene(planningScene);
-        	}
-        });
+    private void switchScene(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.A)) {
+            Parent temp = this.activeScene.getRoot();
+            this.activeScene.setRoot(sleepingRoot);
+            this.sleepingRoot = temp;
+        }
     }
 }
