6e0acf39d643939315beb43ce79ebc1ae356ad6d
Luca Pasini <pasini.luca09@gmail.com>
2022-05-28 16:56:20 +0200

    Create PlanningChannelsView

---
+++ app/src/main/java/view/planning/PlanningChannelsView.java
@@ -0,0 +1,25 @@
+package view.planning;
+
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+import view.common.ChannelsView;
+import view.common.TimeAxisSetter;
+import view.common.ViewDataImpl;
+
+public class PlanningChannelsView extends ChannelsView {
+    public PlanningChannelsView(TimeAxisSetter timeAxisSetter, VBox channelsContentPane, VBox channelsInfoPane) {
+        super(timeAxisSetter, channelsContentPane, channelsInfoPane);
+    }
+
+    @Override
+    public Node drawClip(ViewDataImpl.Clip clip) {
+        return new Label(clip.getTitle());
+    }
+
+    @Override
+    public Node drawInfos(ViewDataImpl.Channel ch) {
+        return new FlowPane();
+    }
+}
