d35453a113246046ecce60ef64d2d56cf48b9a70
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-05-29 15:33:07 +0200

    Register new modules in AbstractJacksonDeserializer

--- app/src/main/java/controller/storing/deserialization/AbstractJacksonDeserializer.java
+++ app/src/main/java/controller/storing/deserialization/AbstractJacksonDeserializer.java
@@ -4,8 +4,9 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import daw.core.mixer.RPMixer;
-import daw.manager.ChannelLinker;
+import daw.core.channel.RPChannel;
+import daw.manager.RPChannelLinker;
+import planning.RPRole;
 
 public abstract class AbstractJacksonDeserializer<T> implements Deserializer<T> {
 
@@ -14,7 +15,11 @@ public abstract class AbstractJacksonDeserializer<T> implements Deserializer<T> 
     protected AbstractJacksonDeserializer() {
         this.mapper = new JsonMapper().builder()
                 .build()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .registerModule(new SimpleModule()
+                        .addDeserializer(RPChannel.class, new ChannelDeserializer())
+                        .addDeserializer(RPChannelLinker.class, new ChannelLinkerDeserializer())
+                        .addKeyDeserializer(RPRole.class, new RoleDeserializer()));
     }
 
     @Override
