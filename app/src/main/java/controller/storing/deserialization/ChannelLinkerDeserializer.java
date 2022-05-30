91da3e4a6bc48ec2bfecc76a722698d803dd40ec
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-05-30 12:25:52 +0200

    Mock implementation of ChannelLinkerDeserializer

--- app/src/main/java/controller/storing/deserialization/ChannelLinkerDeserializer.java
+++ app/src/main/java/controller/storing/deserialization/ChannelLinkerDeserializer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
 import daw.manager.ChannelLinker;
 import java.io.IOException;
 
@@ -11,7 +12,9 @@ public class ChannelLinkerDeserializer extends JsonDeserializer<ChannelLinker> {
 
     @Override
     public ChannelLinker deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
-        return new ChannelLinker();
+        JsonNode node = p.getCodec().readTree(p);
+        System.out.println(node.toPrettyString());
+        return null;
     }
 
 }
