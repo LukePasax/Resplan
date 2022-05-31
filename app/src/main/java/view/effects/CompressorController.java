62980e2ede3e257fab5e8bf61e2c4dd586cb9067
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-05-31 10:42:18 +0200

    Add compressor in CompressorPane

--- app/src/main/java/view/effects/CompressorController.java
+++ app/src/main/java/view/effects/CompressorController.java
@@ -1,11 +1,17 @@
 package view.effects;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 
-public class CompressorController {
+public class CompressorController implements Initializable {
+	
+	private final static double lowerbound = -100.0;
+	private final static double upperbound = 0.0;
 	
 	@FXML
 	Label lcurrent;
@@ -19,13 +25,19 @@ public class CompressorController {
 	@FXML
 	public void refresh() {
 		value = Double.parseDouble(fvalue.getText());
-		if(value >= -100 && value <= 0) {
-			compressor.setProgress(value);
+		if(value >= lowerbound && value <= upperbound) {
+			compressor.setProgress(-value/(-lowerbound));
 			lcurrent.setText("" + value);
-		} else if (value < -100) {
-			compressor.setProgress(-100.0);
+		} else if (value < lowerbound) {
+			compressor.setProgress(-lowerbound);
 			lcurrent.setText("-inf");
 		}
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		compressor.setProgress(upperbound);
+		
 	}
 	
 }
--- app/src/main/java/view/effects/CompressorPane.java
+++ app/src/main/java/view/effects/CompressorPane.java
@@ -2,8 +2,8 @@ package view.effects;
 
 import java.io.IOException;
 import java.net.URL;
-
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -13,20 +13,23 @@ public class CompressorPane extends VBox {
 	public CompressorPane() throws IOException {
 		final HBox firstrow = new HBox();
 		final HBox secondrow = new HBox();
+		final HBox thirdrow = new HBox();
 		
 		final FXMLLoader loader = new FXMLLoader();
 		final URL url = this.getClass().getResource("/view/Compressor.fxml");
 		loader.setLocation(url);
 		final Pane compressor = loader.load();
+		firstrow.setAlignment(Pos.CENTER);
+		firstrow.getChildren().add(compressor);
 		
 		final KnobPane threshold = new KnobPane(Double.NEGATIVE_INFINITY, 0, "THRESHOLD", KnobType.OTHER);
 		final KnobPane attack = new KnobPane(0.2, 20, "ATTACK", KnobType.OTHER);
-		firstrow.getChildren().addAll(threshold, attack);
+		secondrow.getChildren().addAll(threshold, attack);
 		
 		final KnobPane ratio = new KnobPane(Double.NEGATIVE_INFINITY, 0, "RATIO", KnobType.RATIO);
 		final KnobPane decay = new KnobPane(0.2, 40, "DECAY", KnobType.OTHER);
-		secondrow.getChildren().addAll(ratio, decay);
+		thirdrow.getChildren().addAll(ratio, decay);
 		
-		this.getChildren().addAll(firstrow, secondrow);
+		this.getChildren().addAll(firstrow, secondrow, thirdrow);
 	}
 }
