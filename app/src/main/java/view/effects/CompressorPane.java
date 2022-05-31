c6735f8043a0fc519c6564f0da96ce496a9d2fa0
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-05-31 11:01:36 +0200

    Complete CompressorPane.java

--- app/src/main/java/view/effects/CompressorPane.java
+++ app/src/main/java/view/effects/CompressorPane.java
@@ -2,18 +2,27 @@ package view.effects;
 
 import java.io.IOException;
 import java.net.URL;
+
+import daw.core.channel.RPChannel;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import view.effects.Knob.KnobType;
 
-public class CompressorPane extends VBox {
+public class CompressorPane extends BorderPane {
 	public CompressorPane() throws IOException {
 		final HBox firstrow = new HBox();
 		final HBox secondrow = new HBox();
 		final HBox thirdrow = new HBox();
+		final VBox firstcolumn = new VBox();
+		final VBox secondcolumn = new VBox(20);
 		
 		final FXMLLoader loader = new FXMLLoader();
 		final URL url = this.getClass().getResource("/view/Compressor.fxml");
@@ -30,6 +39,26 @@ public class CompressorPane extends VBox {
 		final KnobPane decay = new KnobPane(0.2, 40, "DECAY", KnobType.OTHER);
 		thirdrow.getChildren().addAll(ratio, decay);
 		
-		this.getChildren().addAll(firstrow, secondrow, thirdrow);
+		firstcolumn.getChildren().addAll(firstrow, secondrow, thirdrow);
+		
+		final Label title = new Label("SIDECHAIN");
+		final ComboBox<String> channels = new ComboBox<>();
+		channels.getItems().addAll(FXCollections.observableArrayList("channel1", "channel2", "channel3")); //TO COMPLETE with real channels
+		final Button switcher = new Button("Disable");
+		switcher.setOnMouseClicked(e -> {
+			if(switcher.getText().equals("Disable")) {
+				switcher.setText("Enable");
+				channels.setDisable(true);
+			} else {
+				switcher.setText("Disable");
+				channels.setDisable(false);
+			}
+		});
+		
+		secondcolumn.setAlignment(Pos.CENTER);
+		secondcolumn.getChildren().addAll(title, channels, switcher);
+		
+		this.setCenter(firstcolumn);
+		this.setRight(secondcolumn);
 	}
 }
