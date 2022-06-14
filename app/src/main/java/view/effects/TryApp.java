68bbbed9335ab3631b8e40ea63f29637a5363550
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-06-14 11:57:11 +0200

    Change EffectsPane.java

--- app/src/main/java/view/effects/TryApp.java
+++ app/src/main/java/view/effects/TryApp.java
@@ -13,15 +13,6 @@ public class TryApp extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		HBox root = new HBox();
-		/*ContinuousKnobPane cont = new ContinuousKnobPane(Double.NEGATIVE_INFINITY,100.0,5, "Volume");
-		JerkyKnobPane jerky = new JerkyKnobPane(List.of("1:1", "2:2", "3:3", "4:4"), "Ratio");
-		CompressorPane comp = new CompressorPane(-100.0, 0.0);
-		comp.setValue(-60);*/
-		/*CompressorPane cpane = new CompressorPane();
-		cpane.setValue(-60.0);
-		LimiterPane lpane = new LimiterPane();
-		PassPane ppane = new PassPane();
-		ReverbPane rpane = new ReverbPane();*/
 		EffectsPane effects = new EffectsPane();
 		root.getChildren().addAll(effects);
 		Scene scene = new Scene(root);
