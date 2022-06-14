c7bb4a1afab43dade85a99ebc01b079307da4629
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-06-14 12:20:51 +0200

    Add listener in Effectspane.java

--- app/src/main/java/view/effects/EffectsPane.java
+++ app/src/main/java/view/effects/EffectsPane.java
@@ -2,40 +2,47 @@ package view.effects;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.collections.ListChangeListener;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import view.common.App;
+import view.common.ViewDataImpl.Effect;
 
 public class EffectsPane extends VBox {
+	
+	private final Map<String, Pane> effects = createEffects();
+	private final EffectsPane rootPane = this;
 
-	public EffectsPane() {
+	public EffectsPane(final String channel) {
+		App.getData().getChannel(channel).addFxListListener(new ListChangeListener<Effect>() {
+
+			@Override
+			public void onChanged(Change<? extends Effect> c) {
+				c.next();
+				if(c.wasAdded()) {
+					c.getAddedSubList().forEach(e -> rootPane.getChildren().add(effects.get(e.getType())));
+				}
+				
+				if(c.wasRemoved()) {
+					c.getAddedSubList().forEach(e -> rootPane.getChildren().remove(effects.get(e.getType())));
+				}
+				
+				if(c.wasPermutated()) {
+					c.getAddedSubList().forEach(e -> {
+						rootPane.getChildren().set(rootPane.getChildren().indexOf(effects.get(e.getType())), effects.get(e.getType()));
+					});
+				}
+			}
+			
+		});
+	}
+	
+	private Map<String, Pane> createEffects(){
 		Map<String, Pane> effects = new HashMap<>();
 		effects.put("Compressor", new CompressorPane());
 		effects.put("Limiter", new LimiterPane());
 		effects.put("Pass", new PassPane());
 		effects.put("Reverb", new ReverbPane());
-		ObservableList<String> addFX = FXCollections.observableArrayList("Compressor", "Limiter", "Pass", "Reverb");
-		ObservableList<String> rmFX = FXCollections.observableArrayList();
-		ComboBox<String> addEffects = new ComboBox<>(addFX);
-		Button add = new Button("Add effect");
-		ComboBox<String> removeEffects = new ComboBox<>(rmFX);
-		Button remove = new Button("Remove effect");
-		this.getChildren().addAll(addEffects, add, removeEffects, remove);
-		add.setOnMouseClicked(e -> {
-			var element = addEffects.getSelectionModel().getSelectedItem();
-			this.getChildren().add(effects.get(element));
-			addFX.remove(element);
-			rmFX.add(element);
-		});
-		remove.setOnMouseClicked(e -> {
-			var element = removeEffects.getSelectionModel().getSelectedItem();
-			this.getChildren().remove(effects.get(element));
-			addFX.add(element);
-			rmFX.remove(element);
-		});
+		return effects;
 	}
 }
