8003268f752ce0e373184bb64ef53adcc731f130
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-06-15 17:46:35 +0200

    Fix Recorder

--- app/src/main/java/daw/core/clip/Recorder.java
+++ app/src/main/java/daw/core/clip/Recorder.java
@@ -26,6 +26,7 @@ public class Recorder implements RPRecorder {
 
 	@Override
 	public void record() {
+		ac.start();
 		recorder.start();
 	}
 
@@ -33,6 +34,7 @@ public class Recorder implements RPRecorder {
 	public void pause() {
 		recorder.pause(true);
 		recorder.clip();
+		ac.stop();
 	}
 
 	@Override
