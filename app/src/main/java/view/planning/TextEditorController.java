c06cb0ac4efd9b215d7ef381219ed6922fb9a2e9
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-06-17 16:22:30 +0200

    Add separator in TextEditorView.fxml and fix TextEditorController

--- app/src/main/java/view/planning/TextEditorController.java
+++ app/src/main/java/view/planning/TextEditorController.java
@@ -1,12 +1,10 @@
 package view.planning;
 
 import Resplan.Starter;
-import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import view.common.AlertDispatcher;
 import view.common.TextFilePicker;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -16,10 +14,7 @@ public class TextEditorController {
     public Button editButton;
     public Button saveButton;
     private String clipTitle;
-    public Button upload;
-
-    public TextEditorController() {
-    }
+    public Button uploadButton;
 
     public void setClipTitle(String clipTitle) {
         this.clipTitle = clipTitle;
@@ -27,32 +22,46 @@ public class TextEditorController {
     }
 
     public void initialize() {
-        this.saveButton.setDisable(true);
         this.text.setEditable(false);
+        this.saved();
     }
 
-    public void editText(ActionEvent actionEvent) {
+    public void editText() {
         this.text.setEditable(true);
+        this.editing();
+    }
+
+    private void editing() {
+        this.editButton.setDisable(true);
+        this.uploadButton.setDisable(true);
         this.saveButton.setDisable(false);
     }
 
-    public void saveText(ActionEvent actionEvent) {
+    public void saveText() {
         Starter.getController().setClipText(this.clipTitle, this.text.getText());
-        this.editButton.setDisable(false);
         this.text.setEditable(false);
+        this.saved();
+    }
+
+    private void saved() {
+        this.editButton.setDisable(false);
+        this.uploadButton.setDisable(false);
         this.saveButton.setDisable(true);
     }
 
-    public void uploadFromFile(ActionEvent actionEvent) {
+    public void uploadFromFile() {
         final TextFilePicker picker = new TextFilePicker();
         final File file = picker.getFileChooser().showOpenDialog(this.text.getScene().getWindow());
         if (file != null) {
             try {
                 Starter.getController().uploadTextFromFile(this.clipTitle, file.getAbsolutePath());
+                this.text.setText(Starter.getController().getClipText(this.clipTitle));
             } catch (IOException e) {
                 AlertDispatcher.dispatchError(e.getMessage());
             }
         }
+        this.text.setEditable(true);
+        this.editing();
     }
 
 }
