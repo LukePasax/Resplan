af6e16bf4aa6df7d06c0def7f577644ae23a90f1
Luca Pasini <pasini.luca09@gmail.com>
2022-06-22 14:10:30 +0200

    Improve code style of AlertDispatcher

--- app/src/main/java/view/common/AlertDispatcher.java
+++ app/src/main/java/view/common/AlertDispatcher.java
@@ -3,18 +3,19 @@ package view.common;
 import javafx.scene.control.Alert;
 
 /**
- * This class is used to dispatch {@link Alert}
+ * This class is used to dispatch {@link Alert}.
  */
 public final class AlertDispatcher {
 
-    private AlertDispatcher(){}
+    private AlertDispatcher() { }
 
     /**
      * This method dispatches an {@link Alert} of the {@link javafx.scene.control.Alert.AlertType} ERROR with the given
-     * message
+     * message.
+     *
      * @param message the message that wants to be displayed
      */
-    static public void dispatchError(final String message) {
+    public static void dispatchError(final String message) {
         final Alert error = new Alert(Alert.AlertType.ERROR);
         error.setContentText(message);
         error.setTitle("Error");
