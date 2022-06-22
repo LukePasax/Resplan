f2534691809b5c04f43163664c9360ffe333d8a3
Luca Pasini <pasini.luca09@gmail.com>
2022-06-22 10:45:56 +0200

    Improve code quality of NumberFormatConverter

--- app/src/main/java/view/common/NumberFormatConverter.java
+++ app/src/main/java/view/common/NumberFormatConverter.java
@@ -8,11 +8,13 @@ import java.util.function.UnaryOperator;
 
 public final class NumberFormatConverter extends StringConverter<Number> {
 
+    public static final String SECONDS_FORMAT = "%02d";
+    public static final String REGEX = "^0*";
     private final UnaryOperator<Change> formatterUnaryOperator;
 
     public NumberFormatConverter() {
         this.formatterUnaryOperator = change -> {
-            String text = change.getText();
+            final String text = change.getText();
 
             if (text.matches("[\\d:]*")) {
                 return change;
@@ -24,28 +26,28 @@ public final class NumberFormatConverter extends StringConverter<Number> {
 
     @Override
     public String toString(final Number milliseconds) {
-        Long min = TimeUnit.MILLISECONDS.toMinutes(milliseconds.longValue());
+        final Long min = TimeUnit.MILLISECONDS.toMinutes(milliseconds.longValue());
         long sec = TimeUnit.MILLISECONDS.toSeconds(milliseconds.longValue());
-        Long ms = milliseconds.longValue() - TimeUnit.SECONDS.toMillis(sec);
+        final Long ms = milliseconds.longValue() - TimeUnit.SECONDS.toMillis(sec);
         sec -= TimeUnit.MINUTES.toSeconds(min);
         if (ms.equals(0L)) {
-            return String.format("%02d", min) + ":" + String.format("%02d", sec);
+            return String.format(SECONDS_FORMAT, min) + ":" + String.format(SECONDS_FORMAT, sec);
         }
-        return String.format("%02d", min) + ":" + String.format("%02d", sec) + ":" + ms;
+        return String.format(SECONDS_FORMAT, min) + ":" + String.format(SECONDS_FORMAT, sec) + ":" + ms;
     }
 
     @Override
     public Number fromString(final String string) {
-        String[] s = string.split(":");
-        long min = s.length >= 1
-               ? Long.decode(s[0].replaceFirst("^0*", "").isEmpty()
-                       ? "0" : s[0].replaceFirst("^0*", "")) : 0;
-        long sec = s.length >= 2 
-               ? Long.decode(s[1].replaceFirst("^0*", "").isEmpty()
-                       ? "0" : s[1].replaceFirst("^0*", "")) : 0;
-        long ms = s.length >= 3 
-               ? Long.decode(s[2].replaceFirst("^0*", "").isEmpty()
-                       ? "0" : s[2].replaceFirst("^0*", "")) : 0;
+        final String[] s = string.split(":");
+        final long min = s.length >= 1
+               ? Long.decode(s[0].replaceFirst(REGEX, "").isEmpty()
+                       ? "0" : s[0].replaceFirst(REGEX, "")) : 0;
+        final long sec = s.length >= 2
+               ? Long.decode(s[1].replaceFirst(REGEX, "").isEmpty()
+                       ? "0" : s[1].replaceFirst(REGEX, "")) : 0;
+        final long ms = s.length >= 3
+               ? Long.decode(s[2].replaceFirst(REGEX, "").isEmpty()
+                       ? "0" : s[2].replaceFirst(REGEX, "")) : 0;
         return TimeUnit.SECONDS.toMillis(
                 TimeUnit.MINUTES.toSeconds(min) + sec) + ms;
     }
