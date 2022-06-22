36d37065888d3af3156ad104a120357102d376fa
Luca Pasini <pasini.luca09@gmail.com>
2022-06-22 10:32:27 +0200

    Improve code quality of AlertDispatcher

--- app/src/main/java/view/common/AlertDispatcher.java
+++ app/src/main/java/view/common/AlertDispatcher.java
@@ -5,15 +5,17 @@ import javafx.scene.control.Alert;
 /**
  * This class is used to dispatch {@link Alert}
  */
-public class AlertDispatcher {
+public final class AlertDispatcher {
+
+    private AlertDispatcher(){}
 
     /**
      * This method dispatches an {@link Alert} of the {@link javafx.scene.control.Alert.AlertType} ERROR with the given
      * message
      * @param message the message that wants to be displayed
      */
-    static public void dispatchError(String message) {
-        Alert error = new Alert(Alert.AlertType.ERROR);
+    static public void dispatchError(final String message) {
+        final Alert error = new Alert(Alert.AlertType.ERROR);
         error.setContentText(message);
         error.setTitle("Error");
         error.showAndWait();
