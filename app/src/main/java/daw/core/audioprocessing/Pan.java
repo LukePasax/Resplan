f285ddd1c145b65af09e492c83b4ee7cc9cf2571
Giacomo Sirri <sirrigiacomo9@gmail.com>
2022-06-23 12:48:04 +0200

    Add javadoc to Pan

--- app/src/main/java/daw/core/audioprocessing/Pan.java
+++ app/src/main/java/daw/core/audioprocessing/Pan.java
@@ -6,6 +6,11 @@ import net.beadsproject.beads.ugens.Gain;
 
 import java.util.Map;
 
+/**
+ * Pan is the element that allows to accomplish audio panning.
+ * Panning is the distribution of an audio signal into a new stereo or multichannel sound field,
+ * as determined by pan's value.
+ */
 public class Pan extends UGen implements AudioElement {
 
     private final Gain gainIn;
@@ -14,6 +19,12 @@ public class Pan extends UGen implements AudioElement {
     private final Gain right;
     private float value;
 
+    /**
+     * Construct a Pan whose power remains constant regardless of its position.
+     * This means that center-panning a signal will yield the input signal multiplied by 1 / sqrt(2)
+     * in each output channel as a result. Setting the value to -1 pans completely to the left,
+     * whereas 1 pans completely to the right and 0 (the default value) results in center panning.
+     */
     public Pan() {
         super(AudioContextManager.getAudioContext());
         this.gainIn = new Gain(AudioContextManager.getAudioContext(), 2, 1.0f);
@@ -30,11 +41,19 @@ public class Pan extends UGen implements AudioElement {
         this.gainOut.addInput(1,this.right,0);
     }
 
+    /**
+     * {@inheritDoc}
+     * @return {@inheritDoc}
+     */
     @Override
     public Map<String, Float> getParameters() {
         return Map.of("value", this.value);
     }
 
+    /**
+     * {@inheritDoc}
+     * @param parameters the {@link Map} that contains the parameters that must be modified.
+     */
     @Override
     public void setParameters(Map<String, Float> parameters) {
         this.value = parameters.get("value");
@@ -42,26 +61,45 @@ public class Pan extends UGen implements AudioElement {
         this.right.setGain((float) Math.sin(Math.PI * (1 + value) / 4));
     }
 
+    /**
+     * {@inheritDoc}
+     * @return {@inheritDoc}
+     */
     @Override
     public int getIns() {
         return this.gainIn.getIns();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return {@inheritDoc}
+     */
     @Override
     public int getOuts() {
         return this.gainOut.getOuts();
     }
 
+    /**
+     * {@inheritDoc}
+     * @return {@inheritDoc}
+     */
     @Override
     public Gain getGainIn() {
         return this.gainIn;
     }
 
+    /**
+     * {@inheritDoc}
+     * @return {@inheritDoc}
+     */
     @Override
     public Gain getGainOut() {
         return this.gainOut;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void calculateBuffer() {
     }
