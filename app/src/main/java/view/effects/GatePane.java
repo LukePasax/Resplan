ca81db8c6e2e9950a7bb41b0ff5cf38441466876
Menghi Gabriele <93596528+GabrieleMenghi@users.noreply.github.com>
2022-06-24 10:15:41 +0200

    Add GatePane.java

---
+++ app/src/main/java/view/effects/GatePane.java
@@ -0,0 +1,175 @@
+package view.effects;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import resplan.Starter;
+
+public final class GatePane extends BorderPane {
+	
+	final private VUMeterPane compressor = new VUMeterPane(Double.NEGATIVE_INFINITY, 0.0);
+	
+	final private VBox effects = new VBox();
+	
+	final private Label thresholdValue = new Label("0.0");
+	final private Label attackValue = new Label("0.2");
+	final private Label ratioValue = new Label("2:1");
+	final private Label decayValue = new Label("0.2");
+	private String channel;
+	private int index;
+	
+	public GatePane(final String title, final String channel, final int index) {
+		this.index = index;
+		this.channel = channel;
+		final HBox titlebox = new HBox();
+		titlebox.setAlignment(Pos.CENTER);
+		final Label ltitle = new Label(title);
+		ltitle.setStyle("-fx-font-weight: bold");
+		titlebox.getChildren().add(ltitle);
+		effects.getChildren().add(titlebox);
+		//VUMiter
+		final HBox firstrow = new HBox();
+		firstrow.getChildren().add(compressor);
+		firstrow.setAlignment(Pos.CENTER);
+		effects.getChildren().add(firstrow);
+		//Threshold
+		final HBox hthreshold = new HBox();
+		final Label threshold = new Label("Threshold: ");
+		hthreshold.getChildren().addAll(threshold, thresholdValue);
+		hthreshold.setAlignment(Pos.CENTER);
+		effects.getChildren().add(hthreshold);
+		//Attack
+		final HBox hattack = new HBox();
+		final Label attack = new Label("Attack: ");
+		hattack.getChildren().addAll(attack, attackValue);
+		hattack.setAlignment(Pos.CENTER);
+		effects.getChildren().add(hattack);
+		//Ratio
+		final HBox hratio = new HBox();
+		final Label ratio = new Label("Ratio: ");
+		hratio.getChildren().addAll(ratio, ratioValue);
+		hratio.setAlignment(Pos.CENTER);
+		effects.getChildren().add(hratio);
+		//Decay
+		final HBox hdecay = new HBox();
+		final Label decay = new Label("Decay: ");
+		hdecay.getChildren().addAll(decay, decayValue);
+		hdecay.setAlignment(Pos.CENTER);
+		effects.getChildren().add(hdecay);
+		
+		final Button expand = new Button("View details");
+		final HBox hbutton = new HBox();
+		hbutton.getChildren().add(expand);
+		HBox.setMargin(expand, new Insets(10,0,0,0));
+		hbutton.setAlignment(Pos.CENTER);
+		effects.getChildren().add(hbutton);
+		
+		expand.setOnMouseClicked(e -> {
+			final Gate gate = new Gate();
+			gate.show(title, Double.parseDouble(thresholdValue.getText()), Double.parseDouble(attackValue.getText()), 
+							Double.parseDouble(decayValue.getText()));
+		});
+		this.setCenter(effects);
+		effects.setPadding(new Insets(10));
+		effects.setAlignment(Pos.CENTER);
+		this.setStyle("-fx-border-color: black");
+		String css = this.getClass().getResource("/stylesheets/planning.css").toExternalForm(); 
+		this.getStylesheets().add(css);
+	}
+	
+	public final void setValue(final Double value) {
+		compressor.setValue(value);
+	}
+	
+	public final class Gate {
+		public final void show(final String title, final double currentThreshold, final double currentAttack, final double currentDecay) {
+			BorderPane root = new BorderPane();
+			final HBox titlebox = new HBox(new Label(title));
+			titlebox.setAlignment(Pos.CENTER);
+			
+			final HBox firstrow = new HBox();
+			final HBox secondrow = new HBox();
+			final VBox firstcolumn = new VBox();
+			
+			final ContinuousKnobPane threshold = new ContinuousKnobPane(Double.NEGATIVE_INFINITY, 0.0, currentThreshold, 0, "THRESHOLD");
+			threshold.getValueProperty().addListener((ch, old, n) -> {
+				setParameters("threshold", n.floatValue());
+			});
+			final ContinuousKnobPane attack = new ContinuousKnobPane(0.2, 20.0, currentAttack, 3, "ATTACK");
+			attack.getValueProperty().addListener((ch, old, n) -> {
+				setParameters("attack", n.floatValue());
+			});
+			firstrow.getChildren().addAll(threshold, attack);
+			
+			final JerkyKnobPane ratio = new JerkyKnobPane(List.of("1:1", "2:1", "4:1", "8:1"), "RATIO");
+			ratio.setValue("2:1");
+			ratio.getValueProperty().addListener((ch, old, n) -> {
+				switch (n) {
+				case "1:1":
+					setParameters("ratio", 1f);
+					break;
+				case "2:1":
+					setParameters("ratio", 2f);
+					break;
+				
+				case "4:1":
+					setParameters("ratio", 4f);
+					break;
+				case "8:1":
+					setParameters("ratio", 8f);
+					break;
+				}
+			});
+			final ContinuousKnobPane decay = new ContinuousKnobPane(0.2, 40.0, currentDecay, 3, "DECAY");
+			decay.getValueProperty().addListener((ch, old, n) -> {
+				setParameters("decay", n.floatValue());
+			});
+			secondrow.getChildren().addAll(ratio, decay);
+			
+			firstcolumn.getChildren().addAll(firstrow, secondrow);
+			
+			root.setTop(titlebox);
+			root.setCenter(firstcolumn);
+			
+			root.setPadding(new Insets(10));
+			root.setStyle("-fx-border-color: black");
+			
+			Stage stage = new Stage();
+			stage.setResizable(false);
+			
+			final HBox hrefresh = new HBox();
+			final Button refresh = new Button("Refresh values and close");
+			hrefresh.setAlignment(Pos.CENTER);
+			hrefresh.getChildren().add(refresh);
+			root.setBottom(hrefresh);
+			refresh.setOnMouseClicked(e -> {
+				thresholdValue.setText("" + threshold.getValue());
+				attackValue.setText("" + attack.getValue());
+				ratioValue.setText(ratio.getValue());
+				decayValue.setText("" + decay.getValue());
+				effects.autosize();
+				stage.close();
+			});
+			Scene scene = new Scene(root);
+			String css = scene.getClass().getResource("/stylesheets/planning.css").toExternalForm(); 
+			scene.getStylesheets().add(css);
+			stage.setScene(scene);
+			stage.show();
+		}
+		
+		private final void setParameters(String par, Float value) {
+			final Map<String, Float> values = new HashMap<>();
+			values.put(par, value);
+			Starter.getController().setEffectParameters(channel, index, values);
+		}
+	}
+}
